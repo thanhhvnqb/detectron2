880b8c8d5c628c2ce23deadeda8051ef9290a2bc
Thanh <thanhhvnqb@gmail.com>
2019-10-31 15:38:52 +0900

    add FCOS backbone

--- projects/PoseNet/posenet/__init__.py
+++ projects/PoseNet/posenet/__init__.py
@@ -1,5 +1,6 @@
 # Copyright (c) Facebook, Inc. and its affiliates. All Rights Reserved
 from .config import add_posenet_config
+from .backbone import build_fcos_resnet_fpn_backbone
 from .posenet_head import PoseConvHead
 from .posenet_rpn import PoseRPNHead, PoseRPN
 from .roi_heads import PoseROIHeads
