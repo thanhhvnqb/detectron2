f3bb45faeb19ea0f14f0ca6eede0c8246e0945bd
Thanh <thanhhvnqb@gmail.com>
2019-11-14 10:10:04 +0900

    clean code of backup

--- projects/PoseNet/train_coco.sh
+++ projects/PoseNet/train_coco.sh
@@ -6,12 +6,8 @@
 # ln -s $Dataset/test2017 datasets/coco/test2017
 # ln -s $Dataset/val2017 datasets/coco/val2017
 
-# netname='kprcnn_fcos'
-# netname='kprcnn_mod'
-# netname='kprcnn_mod_fcos'
-netname='posenet_fcos_conv'
-# netname='posenet_fcos_fc'
-# netname='posenet_rcnn_fc'
+netname='kprcnn_fcos'
+# netname='posenet_fcos_conv'
 run_date=$(date +%Y%m%d)
 # run_date=201910930
 # test_conf=instant_test
@@ -21,21 +17,9 @@ outlog=out/run_${run_date}_$netname.log
 if [ "$netname" = 'kprcnn_fcos' ];
 then
     configfile=configs/kprcnn_R_50_fcos_FPN_$test_conf.yaml
-elif [ "$netname" = 'kprcnn_mod' ];
-then
-    configfile=configs/kprcnn_mod_R_50_FPN_$test_conf.yaml
-elif [ "$netname" = 'kprcnn_mod_fcos' ];
-then
-    configfile=configs/kprcnn_mod_R_50_fcos_FPN_$test_conf.yaml
 elif [ "$netname" = 'posenet_fcos_conv' ];
 then
     configfile=configs/posenet_fcos_conv_R_50_FPN_$test_conf.yaml
-elif [ "$netname" = 'posenet_fcos_fc' ];
-then
-    configfile=configs/posenet_fcos_fc_R_50_FPN_$test_conf.yaml
-elif [ "$netname" = 'posenet_rcnn_fc' ];
-then
-    configfile=configs/posenet_rcnn_fc_R_50_FPN_$test_conf.yaml
 fi
 
 if test -f "$outlog"; then
